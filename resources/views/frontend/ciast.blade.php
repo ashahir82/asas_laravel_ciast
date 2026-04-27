0d97c9be914ac196a5757088da120c96ebb88afb
ashahir <ashahir@gmail.com>
2026-04-27 16:39:50 +0800

    pelajar dan pengajar

---
+++ resources/views/frontend/ciast.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CIAST</title>
+</head>
+<body>
+    <h1>SELAMAT DATANG KE CIAST</h1>
+    <p>
+        <a href="/">Utama</a>
+        <a href="/student">Pelajar</a>
+        <a href="/lecturer">Pengajar</a>
+    </p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\LecturerController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
-    return view('welcome');
+    //vreturn view('welcome');
+    return view('frontend.ciast');
 });
+
+Route::get('/admin', function () {
+    return view('backend.admin');
+});
+
+Route::get('/student', [StudentController::class, 'index'])->name('student.index');
+
+Route::get('/lecturer', [LecturerController::class, 'index'])->name('lecturer.index');
+
