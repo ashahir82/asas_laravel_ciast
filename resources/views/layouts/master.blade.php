b8b60b683a87ea28b127149de427b5c577ab9470
ashahir <ashahir@gmail.com>
2026-04-28 10:49:37 +0800

    add bootstrap

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Asas Laravel</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 </head>
 <body>
     <header>
@@ -12,5 +13,6 @@
     <main>
         @yield('content')
     </main>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
